010957ed0e8d7648fd6fabaabbda919523b7b82e
krsnajeeva <krsnajeeva98@gmail.com>
2025-11-18 03:10:39 +0530

    updating github and linkedin profile

--- src/data/contact.jsx
+++ src/data/contact.jsx
@@ -4,12 +4,12 @@ export const contact = {
   role: "Senior Engineer | React Native | Redux | Hooks | Typescript | Javascript",
   sortRole: "Senior Engineer | React Native ",
   email: "krsnajeeva98@gmail.com",
-  github: "https://github.com/yourgithub",
-  linkedin: "https://linkedin.com/in/yourlinkedin",
+  github: "https://github.com/krsnajeeva",
+  linkedin: "https://www.linkedin.com/in/jeevanandham-krishnakumar-606231189/",
 
   shortSummary: (
     <>
-      Experienced Senior Engineer with 6 years in software development,
+      Experienced Senior Engineer with 6 years 4 months in software development,
       specializing in mobile and OTT platforms using{" "}
       <span className="font-medium text-gray-800">React Native</span> and{" "}
       <span className="font-medium text-gray-800">Android</span>.
